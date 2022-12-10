4092ed6a061d73eb774100ecd95f1527c5a1fbae
Yaroslaizerone <kolpackov.yarosl@yandex.ru>
2022-12-10 23:46:42 +0700

    kk gg bro all project ready

--- app/src/main/java/com/example/pr26/data.java
+++ app/src/main/java/com/example/pr26/data.java
@@ -25,6 +25,7 @@ public class data extends AppCompatActivity {
         buttonlist = findViewById(R.id.button);
         buttonlist.setVisibility(View.GONE);
         textView.setText("");
+        int schet = 1;
         while(query.moveToNext()){
             String name_order = query.getString(0);
             String cost = query.getString(1);
@@ -36,9 +37,10 @@ public class data extends AppCompatActivity {
             String name_client = query.getString(7);
             String phone_client = query.getString(8);
 
-            textView.append(name_order+" "+cost+" "+add_item+" "+sugar+" "+adder+" "+personaldeliv+" "+deliv+" "+name_client+" "+phone_client);
+            textView.append("           Заказ#" +schet + "                 \n ");
+            textView.append("Название блюда: "+name_order+"\n Cтоимость: "+cost+"\n Добавка: "+add_item+"\n Сахарку?: "+sugar+"\n Топинг?: "+adder+"\n Самовывоз?: "+personaldeliv+"\n Доставочку?: "+deliv+"\n Имя: "+name_client+"\n Телефонный номер: "+phone_client);
             textView.append(" \n ");
-            textView.append(" \n ");
+            schet++;
         }
         query.close();
         db.close();
